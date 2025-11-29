13f12518010eb21e80507c45f2632ed9eeef2b20
G6B0 <146021873+G6B0@users.noreply.github.com>
2025-11-29 12:53:57 -0300

    Implementación de Rank y select
    Se implementó la función Rank(x) y Select(r)

--- MRL_sketch.cpp
+++ MRL_sketch.cpp
@@ -27,7 +27,46 @@ void MRL_sketch::insertar(int i){
         }
 
         A[j].clear();
+        B.clear();
         j++;
     }
 }
 
+int MRL_sketch::rank(int x){
+    int ans = 0;
+    for (size_t j = 0; j < L; j++)
+    {
+        for (size_t z = 0; z < A[j].size(); z++)
+        {
+            if (A[j][z] < x){
+                ans = ans + (1 << j);
+            }
+        }
+    }
+    return ans;
+}
+
+int MRL_sketch::select(int r){
+    if(B.empty()){
+        for (size_t j = 0; j <= L; j++){
+            for (size_t z = 0; z < A[j].size(); z++){
+                int valor = A[j][z];
+                B.emplace_back(valor, 1 << j);
+            }
+        }     
+    }
+    std::sort(B.begin(), B.end(), [](const auto &a, const auto &b){
+        return a.first < b.first;
+    });
+    int a = 0;
+    int primero = -1;
+    for (size_t t = 0; t < B.size(); t++)
+    {
+        a = a + B[t].second;
+        if(a >= r){
+            primero = B[t].first;
+            break;
+        } 
+    }
+    return primero;
+}
